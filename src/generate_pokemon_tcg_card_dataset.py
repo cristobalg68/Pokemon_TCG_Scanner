8e9707bf5335774f5c0f9e9b6fc4659e9b5f3703
cristobalg68 <cristobal.sebastian.guerra@gmail.com>
2025-03-27 09:40:49 -0300

    A log is added to see the status of the extraction and the error handling related to the image of a card, either because of the name of the card or because a card object does not have a key named image, is improved.

--- src/generate_pokemon_tcg_card_dataset.py
+++ src/generate_pokemon_tcg_card_dataset.py
@@ -2,7 +2,11 @@ import pandas as pd
 import requests
 import os
 import json
+import re
 
+def sanitize_filename(filename):
+    invalid_chars = r'[<>:"/\\|?*\x00-\x1F]'
+    return re.sub(invalid_chars, '_', filename)
 
 def creation_db_cards(dir):
 
@@ -14,13 +18,19 @@ def creation_db_cards(dir):
             'completed_set': [],
             'error_with_a_set': [],
             'error_with_a_symbol_set': [],
+            'set_without_symbol': [],
             'error_with_a_logo_set': [],
+            'set_without_logo': [],
             'error_with_a_card': [],
+            'card_without_image': [],
             'error_with_a_image_card': [],
         }
         with open(os.path.join(dir, 'log.json'), 'w') as file:
             json.dump(log_data, file, indent=4)
-    list_cards = []
+
+        init_df = pd.DataFrame([], columns=['ID','Local_ID','Set_ID','Set_Name','Name','Rarity','Firt_Edition','Holo','Normal','Reverse','Promo','Image_Card_URL'])
+        init_df.to_excel(os.path.join(dir, 'cards_of_pokemon.xlsx'), index=False)
+
     response_1 = requests.get('https://api.tcgdex.net/v2/en/sets', headers='')
     if response_1.status_code == 200:
         sets = response_1.json()  
@@ -31,40 +41,68 @@ def creation_db_cards(dir):
                 response_2 = requests.get(f'https://api.tcgdex.net/v2/en/sets/{set_id}', headers='')
                 if response_2.status_code == 200:
                     set = response_2.json()
+                    list_cards = []
+                    safe_filename = sanitize_filename(f'{set_name}.png')
     
                     if 'symbol' in set:
                         image_symbol_url = set['symbol'] + '.png'
                         response_4 = requests.get(image_symbol_url)
                         if response_4.status_code == 200:
-                            with open(os.path.join(dir, 'images', 'symbols', f'{set_name}.png'), 'wb') as f:
+                            with open(os.path.join(dir, 'images', 'symbols', safe_filename), 'wb') as f:
                                 f.write(response_4.content)
                         else:
                             log_data['error_with_a_symbol_set'].append(set_id)
                             with open(os.path.join(dir, 'log.json'), 'w') as file:
                                 json.dump(log_data, file, indent=4)
                     else:
-                        print(f'The {set_name} set has no symbol.')
+                        log_data['set_without_symbol'].append(set_id)
+                        with open(os.path.join(dir, 'log.json'), 'w') as file:
+                            json.dump(log_data, file, indent=4)
                     
                     if 'logo' in set:
                         image_logo_url = set['logo'] + '.png'
                         response_5 = requests.get(image_logo_url)
                         if response_5.status_code == 200:
-                            with open(os.path.join(dir, 'images', 'logos', f'{set_name}.png'), 'wb') as f:
+                            with open(os.path.join(dir, 'images', 'logos', safe_filename), 'wb') as f:
                                 f.write(response_5.content)
                         else:
                             log_data['error_with_a_logo_set'].append(set_id)
                             with open(os.path.join(dir, 'log.json'), 'w') as file:
                                 json.dump(log_data, file, indent=4)
                     else:
-                        print(f'The {set_name} set has no logo.')
+                        log_data['set_without_logo'].append(set_id)
+                        with open(os.path.join(dir, 'log.json'), 'w') as file:
+                            json.dump(log_data, file, indent=4)
 
+                    total_cards = len(set['cards'])
+                    k = 0
                     for card in set['cards']:
+                        msn = f'Card {k + 1} / {total_cards} is being extracted from set {set_name}.'
+                        print(msn, end='\r')                                                           ################### arreglar esto
                         card_id = card['id']
                         response_3 = requests.get(f'https://api.tcgdex.net/v2/en/cards/{card_id}', headers='')
                         if response_3.status_code == 200:
                             info_card = response_3.json()
                             card_local_id = card['localId'],
                             card_name = card['name'].replace(' ', '_')
+
+                            if 'image' in info_card:
+                                image_card_url = info_card['image'] + '/high.png'
+                                response_6 = requests.get(image_card_url)
+                                if response_6.status_code == 200:
+                                    safe_filename = sanitize_filename(f'{set_name}_{card_local_id}_{card_name}.png')
+                                    with open(os.path.join(dir, 'images', 'cards', safe_filename), 'wb') as f:
+                                        f.write(response_6.content)
+                                else:
+                                    log_data['error_with_a_image_card'].append((set_id, card_id))
+                                    with open(os.path.join(dir, 'log.json'), 'w') as file:
+                                        json.dump(log_data, file, indent=4)
+                            else:
+                                image_card_url = ''
+                                log_data['card_without_image'].append((set_id, card_id))
+                                with open(os.path.join(dir, 'log.json'), 'w') as file:
+                                    json.dump(log_data, file, indent=4)
+                            
                             list_cards.append({
                                 'ID': card_id, 
                                 'Local_ID': card['localId'], 
@@ -77,32 +115,26 @@ def creation_db_cards(dir):
                                 'Normal': int(info_card['variants']['normal']),
                                 'Reverse': int(info_card['variants']['reverse']),
                                 'Promo': int(info_card['variants']['wPromo']),
+                                'Image_Card_URL': image_card_url,
                                 })
-                            image_card_url = info_card['image'] + '/high.png'
-                            response_6 = requests.get(image_card_url)
-                            if response_6.status_code == 200:
-                                with open(os.path.join(dir, 'images', 'cards', f'{set_name}_{card_local_id}_{card_name}.png'), 'wb') as f:
-                                    f.write(response_6.content)
-                            else:
-                                log_data['error_with_a_image_card'].append((set_id, card_id))
-                                with open(os.path.join(dir, 'log.json'), 'w') as file:
-                                    json.dump(log_data, file, indent=4)
                         else:
                             log_data['error_with_a_card'].append((set_id, card_id))
                             with open(os.path.join(dir, 'log.json'), 'w') as file:
                                 json.dump(log_data, file, indent=4)
+                        k += 1
                                 
                     log_data['completed_set'].append(set_id)
                     with open(os.path.join(dir, 'log.json'), 'w') as file:
                         json.dump(log_data, file, indent=4)
 
+                    old_df = pd.read_excel(os.path.join(dir, 'cards_of_pokemon.xlsx'))
+                    new_df = pd.DataFrame(list_cards)
+                    df = pd.concat([old_df, new_df])
+                    df.to_excel(os.path.join(dir, 'cards_of_pokemon.xlsx'), index=False)
+
                 else:
                     log_data['error_with_a_set'].append(set_id)
                     with open(os.path.join(dir, 'log.json'), 'w') as file:
                         json.dump(log_data, file, indent=4)
-    else:
-        print('Extraction of failure sets.')
-    df = pd.DataFrame(list_cards)
-    df.to_excel(os.path.join(dir, 'cards_of_pokemon.xlsx'), index=False)
 
-creation_db_cards('datasets')
+creation_db_cards('D:/Proyectos/Pokemon_TCG_Scanner/datasets')
