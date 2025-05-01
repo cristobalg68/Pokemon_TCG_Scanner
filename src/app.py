590c56313e24172a80e5d688964ded109dcf2a13
cristobalg68 <cristobal.sebastian.guerra@gmail.com>
2025-05-01 12:09:54 -0400

    Possible errors when selecting a file and when choosing the camera to use are addressed.

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from tkinter import filedialog, messagebox
 
 from scanner import ImageScanner, VideoScanner, LiveScanner
 
-path_weights = "weights/best_2.pt"
+path_weights = "weights/best_1.pt"
 path_df = "D:/Proyectos/Pokemon_TCG_Scanner/datasets/cards_of_pokemon.xlsx"
 hash_size = 16
 size = 640
@@ -11,6 +11,16 @@ confidence = 0.8
 iou = 0.5
 
 FONT = ("Arial", 12)
+ext_file = {
+    "1":("Image File", r"*.png *.jpg *.jpge"),
+    "2":("Video File", r"*.mp4 *.avi")
+            }
+
+default_file_ext = {
+    "1": ".png",
+    "2": ".mp4",
+    "3": ".mp4"
+}
 
 class GUI:
     def __init__(self, root):
@@ -19,12 +29,12 @@ class GUI:
         root.geometry("500x400")
 
         self.mode = tk.StringVar(value="1")
+        self.input_file_ext = ext_file[self.mode.get()]
         self.file = None
         self.save = tk.BooleanVar()
         self.path_save = tk.StringVar()
 
-        self.use_ip = tk.BooleanVar()
-        self.use_camera_pc = tk.BooleanVar()
+        self.live_source = tk.StringVar(value="pc")
         self.ip_entry_var = tk.StringVar()
 
         self.elements = []
@@ -53,16 +63,17 @@ class GUI:
         self.check_save = tk.Checkbutton(self.root, text="Save result", variable=self.save, command=self.toggle_save, font=FONT)
         self.create_element(self.check_save)
 
-        self.button_save = tk.Button(self.root, text="Select folder and name", command=self.select_save, font=FONT)
+        self.button_save = tk.Button(self.root, text="Select folder", command=self.select_save, font=FONT)
         self.create_element(self.button_save)
+        self.button_save.config(state=tk.DISABLED)
 
         self.label_save = tk.Label(self.root, textvariable=self.path_save, font=FONT)
         self.create_element(self.label_save)
 
         self.label_mode3 = tk.Label(self.root, text="Options for Mode Live:", font=FONT)
-        self.check_ip = tk.Checkbutton(self.root, text="Use IP camera", variable=self.use_ip, command=self.update_ui, font=FONT)
+        self.rb_pc = tk.Radiobutton(self.root, text="Use PC camera", variable=self.live_source, value="pc", command=self.update_ui, font=FONT)
+        self.rb_ip = tk.Radiobutton(self.root, text="Use IP camera", variable=self.live_source, value="ip", command=self.update_ui, font=FONT)
         self.entry_ip = tk.Entry(self.root, textvariable=self.ip_entry_var, font=FONT)
-        self.check_pc = tk.Checkbutton(self.root, text="Use PC camera", variable=self.use_camera_pc, command=self.update_ui, font=FONT)
 
         self.button_execute = tk.Button(self.root, text="Execute", command=self.execute, font=FONT)
         self.create_element(self.button_execute)
@@ -72,40 +83,48 @@ class GUI:
     def update_ui(self):
         mode = self.mode.get()
         if mode in ["1", "2"]:
+            self.input_file_ext = ext_file[mode]
             self.button_file.config(state=tk.NORMAL)
             self.hide_mode3_options()
         else:
             self.button_file.config(state=tk.DISABLED)
             self.show_mode3_options()
 
+        if self.live_source.get() == "ip":
+            self.entry_ip.config(state=tk.NORMAL)
+        else:
+            self.entry_ip.config(state=tk.DISABLED)
+
     def show_mode3_options(self):
         self.label_mode3.pack(before=self.button_execute)
-        self.check_ip.pack(before=self.button_execute)
+        self.rb_pc.pack(before=self.button_execute)
+        self.rb_ip.pack(before=self.button_execute)
         self.entry_ip.pack(before=self.button_execute)
-        self.check_pc.pack(before=self.button_execute)
 
     def hide_mode3_options(self):
         self.label_mode3.pack_forget()
-        self.check_ip.pack_forget()
+        self.rb_pc.pack_forget()
+        self.rb_ip.pack_forget()
         self.entry_ip.pack_forget()
-        self.check_pc.pack_forget()
 
     def select_file(self):
-        file = filedialog.askopenfilename(title="Select file")
+        file = filedialog.askopenfilename(title="Select file", filetypes=[self.input_file_ext])
         if file:
             self.file = file
 
     def toggle_save(self):
         if not self.save.get():
             self.path_save.set("")
+            self.button_save.config(state=tk.DISABLED)
+        else:
+            self.button_save.config(state=tk.NORMAL)
 
     def select_save(self):
         if self.save.get():
             folder = filedialog.askdirectory(title="Select folder")
             if folder:
-                name = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=folder, title="Nanme of file")
-                if name:
-                    self.path_save.set(name)
+                name = folder + "/result" + default_file_ext[self.mode.get()]
+                self.path_save.set(name)
 
     def clear_window(self):
         for element in self.elements:
@@ -133,25 +152,18 @@ class GUI:
             scanner = VideoScanner(path_weights, size, confidence, iou, hash_size, path_df, save, path)
             source = self.file
         else:
-            use_ip = self.use_ip.get()
-            use_camera_pc = self.use_camera_pc.get()
+            source = self.live_source.get()
             ip = self.ip_entry_var.get()
 
-            if not use_ip and not use_camera_pc:
-                messagebox.showerror("Error", "You must select at least one video source for Mode 3.")
+            if source not in ["ip", "pc"]:
+                messagebox.showerror("Error", "You must select one video source for Mode 3.")
                 return
-            if use_ip and not ip:
+            if source == "ip" and not ip:
                 messagebox.showerror("Error", "You must enter the IP address of the camera.")
-                return
-            if use_ip and use_camera_pc:
-                messagebox.showerror("Error", "You can only select one video source: IP or PC camera.")
                 return
 
             scanner = LiveScanner(path_weights, size, confidence, iou, hash_size, path_df, save, path)
-            if use_ip:
-                source = ip
-            else:
-                source = '0'
+            source = ip if source == "ip" else '0'
 
         self.clear_window()
         self.result_frame.pack(fill=tk.BOTH, expand=True)
@@ -163,4 +175,4 @@ if __name__ == "__main__":
     root.mainloop()
 
 
-# 'C:/Users/Cristobal/Desktop/test.jpg' / '' / 'http://192.168.1.6:4747/video'
+# 'C:/Users/Cristobal/Desktop/test.jpg' / '' / 'http://192.168.1.28:4747/video'
