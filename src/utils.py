df870d552af64c58fccd4ee4a5f22e03d21bdaa1
cristobalg68 <cristobal.sebastian.guerra@gmail.com>
2025-05-19 11:31:00 -0400

    Optimizations are applied.

--- src/utils.py
+++ src/utils.py
@@ -21,95 +21,67 @@ def read_image(path_image, size):
     return img
 
 def process_detections(detections):
-    new_detections = []
-    if detections.masks != None:
-        for mask, segmentation, bbox in zip(detections.masks.data, detections.masks.xy, detections.boxes.xywh):
-            det = {
-                'bbox': bbox.cpu().numpy().astype(int),
-                'segmentation': segmentation,
-                'mask': mask.cpu().numpy()
-            }
-            new_detections.append(det)
-    return new_detections
+    if detections.masks is None:
+        return []
+    return [
+        {
+            'bbox': bbox.cpu().numpy().astype(int),
+            'segmentation': seg,
+            'mask': mask.cpu().numpy()
+        }
+        for mask, seg, bbox in zip(detections.masks.data, detections.masks.xy, detections.boxes.xywh)
+    ]
 
 def mask_to_card(image, detections):
-    for detection in detections:
-        mask = detection['mask'].astype(np.uint8) * 255
+    for det in detections:
+        mask = (det['mask'].astype(np.uint8)) * 255
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        card = None
-        
         for contour in contours:
             epsilon = 0.1 * cv2.arcLength(contour, True)
-            points = cv2.approxPolyDP(contour, epsilon, True)
-            if len(points) == 4:
-                centroids = np.mean(points, axis=0)
-                corners = np.array(sorted(points, key=lambda x: np.arctan2(x[0][1] - centroids[0][1], x[0][0] - centroids[0][0])))
-
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            if len(approx) == 4:
+                center = np.mean(approx, axis=0)
+                corners = np.array(sorted(approx, key=lambda x: np.arctan2(x[0][1] - center[0][1], x[0][0] - center[0][0])))
                 width = np.linalg.norm(corners[0] - corners[1])
                 height = np.linalg.norm(corners[1] - corners[2])
                 dst = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype=np.float32)
-                M = cv2.getPerspectiveTransform(points.astype(np.float32), dst)
-
+                M = cv2.getPerspectiveTransform(corners.astype(np.float32), dst)
                 card = cv2.warpPerspective(image, M, (int(width), int(height)))
-                
                 if width > height:
                     card = cv2.rotate(card, cv2.ROTATE_90_CLOCKWISE)
-
-                card = cv2.resize(card, (320, 444))
-                card = cv2.flip(card, 1)
-
-        if card is not None:
-            detection['card_image'] = card
+                det['card_image'] = cv2.flip(cv2.resize(card, (320, 444)), 1)
+                break
 
 def hash_image(img, hash_size):
-    img = Image.fromarray(img)
-    img = img.convert('RGB')
-
-    dhash = imagehash.dhash(img, hash_size)
-    phash = imagehash.phash(img, hash_size)
-
-    return f'{dhash}{phash}'
+    pil_img = Image.fromarray(img).convert('RGB')
+    return f'{imagehash.dhash(pil_img, hash_size)}{imagehash.phash(pil_img, hash_size)}'
 
 def hash_cards(detections, hash_size):
-    for detection in detections:
-        if 'card_image' in detection:
-            image_hash = hash_image(detection['card_image'], hash_size)
-            detection['hash'] = image_hash
-            card_image = cv2.rotate(detection['card_image'], cv2.ROTATE_180)
-            image_hash = hash_image(card_image, hash_size)
-            detection['hash_flipped'] = image_hash
+    for det in detections:
+        if 'card_image' in det:
+            det['hash'] = hash_image(det['card_image'], hash_size)
+            flipped = cv2.rotate(det['card_image'], cv2.ROTATE_180)
+            det['hash_flipped'] = hash_image(flipped, hash_size)
 
-def hamming_distance(hash1, hash2):
-    return sum(ch1 != ch2 for ch1, ch2 in zip(hash1, hash2))
+def hamming_distance(h1, h2):
+    return sum(a != b for a, b in zip(h1, h2))
 
-def find_match(hash_a, df):
-    min_sim = 14*6.8
-
-    df['similarity'] = df['hash'].apply(lambda x: hamming_distance(x, hash_a))
+def find_match(h, df, threshold=14*6.8):
+    df['similarity'] = df['hash'].apply(lambda x: hamming_distance(x, h))
     min_row = df.loc[df['similarity'].idxmin()]
-    if min_row['similarity'] < min_sim:
-        card_name = min_row['Name']
-        set_card_name = min_row['Set_Name']
-        local_id = min_row['Local_ID']
-        return f"{card_name} {set_card_name} {local_id}", min_row['similarity']
+    if min_row['similarity'] < threshold:
+        return f"{min_row['Name']} {min_row['Set_Name']} {min_row['Local_ID']}", min_row['similarity']
     return None, None
 
 def match_hashes(detections, df):
-    for detection in detections:
-        if 'hash' in detection:
-            match1, sim1 = find_match(detection['hash'], df)
-            match2, sim2 = find_match(detection['hash_flipped'], df)
-            if match1 is None and match2 is None:
-                continue
-            elif match1 is None:
-                detection['match'] = match2
-            elif match2 is None:
-                detection['match'] = match1
-            else:
-                if sim2 > sim1:
-                    detection['match'] = match1
-                else:
-                    detection['match'] = match2
+    for det in detections:
+        if 'hash' in det:
+            match1, sim1 = find_match(det['hash'], df)
+            match2, sim2 = find_match(det['hash_flipped'], df)
+            if match1 and (not match2 or sim1 <= sim2):
+                det['match'] = match1
+            elif match2:
+                det['match'] = match2
 
 def draw_boxes_and_segmentation(image, x, y, w, h, segmentation, bbox=False):
     if bbox:
@@ -117,8 +89,7 @@ def draw_boxes_and_segmentation(image, x, y, w, h, segmentation, bbox=False):
     overlay = image.copy()
     cv2.polylines(image, [segmentation], isClosed=True, color=(0, 0, 255), thickness=2)
     cv2.fillPoly(overlay, [segmentation], color=(0, 0, 255))
-    alpha = 0.3
-    image[:] = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
+    cv2.addWeighted(overlay, 0.3, image, 0.7, 0, dst=image)
 
 def draw(image, detections):
     for detection in detections:
